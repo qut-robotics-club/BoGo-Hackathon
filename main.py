384e5e54bd2427a55ba9ca497767b077f7a73800
Baidevin <martinsdevin@gmail.com>
2023-09-30 15:07:28 +1000

    Added print supression

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from ChatGPT import ChatGPT
 from Speech2Txt import Speech2Txt
 
+def blockPrint() -> None:
+    import os
+    import sys
+    sys.stdout = open(os.devnull, 'w')
+
+def enablePrint() -> None:
+    import sys
+    sys.stdout = sys.__stdout__
+
 if __name__ == "__main__":
     chatbot = ChatGPT()
     speech = Speech2Txt()
